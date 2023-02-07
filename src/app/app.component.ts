a3231d53b6e05ddc7c7b3382bc86661800faa257
Tanvi <tanvi.n@applify.co>
2023-02-07 10:57:10 +0530

    Image Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from './file-upload.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'uploadImage';
+
+  constructor(private fileUploadService: FileUploadService) {}
+  shortLink: string = '';
+  loading: boolean = false; // Flag variable
+
+  ngOnInit(): void {}
+  file!: File;
+  // On file Select
+  onChange(event: any) {
+    this.file = event.target.files[0];
+    console.log(this.file);
+  }
+
+  // OnClick of button Upload
+  onUpload() {
+    this.loading = !this.loading;
+    console.log(this.file);
+    this.fileUploadService.upload(this.file).subscribe((event: any) => {
+      console.log(typeof event);
+      if (typeof event === 'object') {
+        // Short link via api response
+        console.log('EVENT', event);
+        this.shortLink = event.link;
+
+        this.loading = false; // Flag variable
+      }
+    });
+  }
 }
